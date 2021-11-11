c478a088763a2701ef27c3ea210d171b345344ba
Ashton Sobeck <asobeck@g.clemson.edu>
2021-11-11 15:19:29 -0500

    infra template done

--- automate_backup.sh
+++ automate_backup.sh
@@ -3,6 +3,8 @@
 echo "Starting backup of home dir now!"
 date_of_backup=`date +"%d-%b-%Y"`
 unix_time_now=`date +%s`
-filename="$date_of_backup-$unix_time_now"
+filename="$date_of_backup-$unix_time_now.tar.gz"
 echo $filename
-# tar -czvf ~ /tmp/backup
+bucketname=""
+# tar -czvf /home /tmp/backup/$filename
+echo "Finished backup, running cloud backup..."

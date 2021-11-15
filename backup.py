99b9d865ee6efc04c0890a5654382bda47981f1b
Ashton Sobeck <asobeck@g.clemson.edu>
2021-11-15 18:39:20 -0500

    fix env path

--- backup.py
+++ backup.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-dotenv_path = Path('~/')
+dotenv_path = Path('~/secrets.env')
 load_dotenv(dotenv_path=dotenv_path)
 AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
 AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
